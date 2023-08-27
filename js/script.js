c6a53c0358806d46a580294d28ca6591b8d9df98
Rishab Gautam <84559232+RISHAB-CREATOR@users.noreply.github.com>
2023-08-27 14:30:15 +0530

    .

--- js/script.js
+++ js/script.js
@@ -54,7 +54,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 //typed js
 
 const typed = new Typed('.multiple-text', {
-    strings: ['Movies', 'Series', 'Anime', 'Free', 'Ad-free'],
+    strings: ['Movies', 'Series', 'Anime', 'Anythng for Free', 'Everything Ad-free'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,

3959cb87a6d9e99d60baea9331eb449eaa3548a7
Rishab <84559232+RISHAB-CREATOR@users.noreply.github.com>
2024-01-18 22:41:44 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -54,7 +54,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 //typed js
 
 const typed = new Typed('.multiple-text', {
-    strings: ['Movies.', 'Series.', 'Anime.', 'Anything for Free.', 'Everything Ad-free.'],
+    strings: ['Shows.', 'Anime.', 'Anything for Free.', 'Everything Ad-free.'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
